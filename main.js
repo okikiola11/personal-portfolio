ee85cb5eb13cd02835ef9c3c24f8674e513d23dc
okikiola11 <okikiola.apelehin@gmail.com>
2021-04-06 17:49:17 +0000

    Add additional changes to UI

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 window.addEventListener('scroll', () => {
   const header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0);
-})
+});
+
+const navBrand = document.querySelector('.navbar-brand');
+const header = document.getElementsByTagName('body')[0];
+
+navBrand.textContent = '<She\'Coder/>';
+header.appendChild('navbar-nav'); 
